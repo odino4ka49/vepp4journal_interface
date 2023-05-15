418d39b7ded7e3b4845670b21c2bcb177baa81b0
odino4ka49 <tessamarizza@gmail.com>
2023-05-15 15:10:35 +0700

    test3

--- v4journal/models.py
+++ v4journal/models.py
@@ -5,6 +5,6 @@ def getPvs(pvs):
     pv_dict = {}
     for pv_name in pvs:
         pv_dict[pv_name] = epics.caget(pv_name)
-        print(type(pv_dict[pv_name]))
+        print((pv_dict[pv_name]))
     print(pv_dict)
     return pv_dict
